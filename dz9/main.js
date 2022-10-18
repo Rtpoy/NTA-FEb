50e6fef346993f1f64d6767bd948de40cfbc79b5
Rtpoy <104576500+Rtpoy@users.noreply.github.com>
2022-10-18 18:42:30 +0300

    dz9.1

---
+++ dz9/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.slider-container').bxSlider({
+      speed: 500,
+      responsive :true,
+      auto: true,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.comments-slider').bxSlider({
+      speed: 500,
+      responsive :true,
+      auto: true,
+      controls:false,
+    });
+  });
+
