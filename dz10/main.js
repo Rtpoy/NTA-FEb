3a2bbc21c620aff5906dc376ff4f18f269d56ff0
Rtpoy <104576500+Rtpoy@users.noreply.github.com>
2022-11-01 18:57:44 +0200

    dz10.1

---
+++ dz10/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.recomend-slider').bxSlider({
+        auto: true,
+        controls: true,
+        responsive: true,
+        speed: 1000,
+        pager: false,
+        mimSlides: 1,
+        maxSlides: 5,
+        moveSlides: 1,
+        slideWidth: 366
+    });
+  });
