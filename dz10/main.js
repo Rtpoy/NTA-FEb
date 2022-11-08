eb7b98c562ef0c7ada816fd454f1daa56150679f
Rtpoy <104576500+Rtpoy@users.noreply.github.com>
2022-11-08 19:00:44 +0200

    dz10.4

--- dz10/main.js
+++ dz10/main.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+
+
+    $('.tabs-nav a').on('click',function(){
+    $('.tabs-nav a').removeClass('active');
+    $(this).addClass('active');
+    let href = $(this).attr('href')
+    $('.tab-pane').removeClass('active').removeClass('in');
+    let id = $(href).addClass('active');    
+
+    setTimeout(function(){
+        $(href).addClass('in');
+    }, 200);
+    return  false;
+
+});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
     $('.recomend-slider').bxSlider({
         auto: true,
         controls: true,
